2f6590380ae3358f9d964e9308c696bd5fae7a7b
Alena Borisova <lenbar20@gmail.com>
2019-02-02 18:33:26 +0300

    Basic functionality for the exercise 'word'

--- components/wordForLearn.vue
+++ components/wordForLearn.vue
@@ -1,29 +1,72 @@
 <template>
-  <b-card></b-card>
+  <b-card no-body
+    style="max-width: 20rem;">
+    <h4 slot="header"
+      v-if="_show.title">
+      {{_title}}</h4>
+    <b-list-group flush>
+      <b-list-group-item
+        v-for="answer in sortAnswer(_answers)"
+        :key="answer.id"
+        @click="checkAnswer(answer)">
+        {{answer[_answerLabel].text}}
+      </b-list-group-item>
+    </b-list-group>
+    <b-card-body v-if="_show.audio">
+      <audio controls style="width: 250px;">
+        <source :src="_audio" type="audio/mpeg">
+      </audio>
+    </b-card-body>
+    <!-- <b-card-footer v-if="_showAllMean">
+      <b-button @click="$emit('click')">{{_showAllMean}}
+        </b-button>
+      </b-card-footer> -->
+  </b-card>
 </template>
 
 <script>
 export default {
   name: 'word-for-learn',
   props: {
-    _collection: {
-      type: String
+    _title: { 
+      type: String,
+      default: ''
     },
-    _document: {
-      type: String
+    _answerId:{
+      type: String,
+      default: 'text'
     },
-    _cards: {
+    _answers: {
+      type: Array
+    },
+    _audio: {
+      type: String,
+      default: ''
+    },
+    _show: {
       type: Object
+    },
+    _answerLabel: {
+      type: String
     }
   },
-  computed: {
-    correctAnswer () {
-      
-    }
-  },
+  // computed: {
+  //   answers () {
+  //     return Object.assign(this._answers
+  //   }
+  // },
   methods: {
-    checkAnswer () {
-      return correctAnswer
+    checkAnswer (answer) {
+      debugger
+      if (this._answerId == answer.id) {
+        this.$emit('change', answer)
+      }
+    },
+    sortAnswer(answers) {
+      return answers.slice().sort(this.compareRandom)
+    },
+    compareRandom(a, b) {
+      return Math.random() - 0.5
     }
   }
 }
--- pages/practice/_word/index.vue
+++ pages/practice/_word/index.vue
@@ -1,22 +1,53 @@
 <template>
   <b-container class="text-center">
     <b-button @click="getSkyengMeanings">begin to learn the words</b-button>
-    {{meanings}}
+    <wordForLearn v-if="start" 
+      @change="setMeanId"
+      :_title="meanings[meanId].text"
+      :_audio="meanings[meanId].soundUrl"
+      :_answerId="meanings[meanId].id"
+      :_answers="meanings"
+      :_show="show"
+      _answerLabel="translation" />
   </b-container>
 </template>
 
 <script>
+import wordForLearn from '~/components/wordForLearn.vue'
+
 export default {
   name: 'word',
+  components: {
+    wordForLearn
+  },
+  data () {
+    return {
+      start: false,
+      meanId: 0,
+      show: {
+        title: true,
+        audio: false,
+      }
+    }
+  },
   computed: {
     meanings () {
       return this.$store.state.meanings || {}
     }
   },
   methods: {
+    setMeanId () {
+      if (this.meanings && this.meanId != this.meanings.length - 1)
+        this.meanId += 1
+    },
     getSkyengMeanings () {
       this.$store.dispatch('getSkyengMeanings', this.randomIds()).then(() => {
-        return this.$store.state.meanings
+        if( this.$store.state.meanings.length === 5) {
+          this.start = true
+          return this.$store.state.meanings
+        } else {
+          this.getSkyengMeanings()
+        }
       })
     },
     randomIds () {
