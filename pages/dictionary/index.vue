8e0a0efc07b76cba022b43c06b48aa572b44310b
Alena Borisova <lenbar20@gmail.com>
2019-02-06 20:12:55 +0300

    added the page 'dictionary'

--- pages/dictionary/index.vue
+++ pages/dictionary/index.vue
@@ -1,24 +1,59 @@
 <template>
   <b-container class="text-center">
-    <!-- <b-button  @click="getWord"> test </b-button> -->
+    <h2  class="my-5">Dictionary</h2>
+    <loading v-if="loading"/>
+    <b-row class="justify-content-center" v-if="!loading">
+      <b-col v-for="word in wordsForDictionary" :key="word.id">
+    <cardWord
+      class="mb-3"
+      :_imageWord="getMeaningImg(word)"
+      :_title="word.text"
+      :_text="word.translation.text"
+      :_transcription="word.transcription"
+      :_audio="word.soundUrl"/>
+    </b-col>
+    </b-row>
   </b-container>
 </template>
 
 <script>
-
+import cardWord from '~/components/CardWord.vue'
+import loading from '~/components/loading.vue'
 export default {
   name: 'dictionary',
-  data () {
-    return {
-      word: null
+  components: {
+    cardWord,
+    loading
+  },
+  created () {
+    this.getWord()
+  },
+  computed: {
+    user () {
+      return this.$store.state.user
+    },
+    loading () {
+      return this.$store.state.loading
+    },
+    wordsForDictionary () {
+      return this.$store.state.wordsForDictionary || {}
     }
   },
   methods: {
     getWord () {
-        this.$store.dispatch('getSkyengWord', 'test').then(() => {
-          this.word = this.$store.state.word
-      })
-    }
+      if (this.user) {
+        this.$store.dispatch('getWordsFromDB').then(() => {
+          return this.$store.state.wordsForDictionary
+        })
+      }
+    },
+    getMeaningImg (mean) {
+      if (mean.images.length != 0) {
+        return mean.images[0].url
+      } else {
+        return ''
+      }
+    },
   }
 }
 </script>
