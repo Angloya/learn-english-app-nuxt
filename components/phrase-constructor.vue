5a25d8cc0e100eaf8fe293fe2c750c2fb345debd
Alena Borisova <alena.borisova@altenar.com>
2019-02-22 17:25:21 +0300

    added new exercise phrase-audio

---
+++ components/phrase-constructor.vue
@@ -0,0 +1,110 @@
+<template>
+  <b-card no-body
+    :bg-variant="_color ? _color : isColorDark ? 'dark' : 'light'"
+    :text-variant="isColorDark ? 'light' : 'dark'"
+    style="max-width: 60rem; min-width: 20rem;">
+    <b-container slot="header" class="px-0">
+      <b-row class="text-center">
+        <h3 v-if="_check"> {{_answer}}</h3>
+        <b-col class="px-0">
+          <i slot="header"
+            v-if="!_check"
+            @click="resetwords"
+            class="material-icons md-light">cached</i>
+        </b-col>
+        </b-row>
+    </b-container>
+    <b-card-body v-if="_show.audio">
+      <audio controls style="width: 250px;">
+        <source :src="_audio" type="audio/mpeg">
+      </audio>
+    </b-card-body>
+    <b-card-body>
+      <b-row class="text-center py-5" v-if="!_check">
+        <b-col v-for="(word, idx) in words" :key="idx" class="px-1">
+          <b-button
+            size="lg"
+            class='word-constructor-word-button my-2 mx-2'
+            @click="setword(idx)">
+            {{word}}
+          </b-button>
+        </b-col>
+      </b-row>
+    </b-card-body>
+     <b-card
+      :bg-variant="_color ? _color : isColorDark ? 'dark' : 'light'"
+      :text-variant="isColorDark ? 'light' : 'dark'"
+      :title="_check ? _userAnswer : answer"/>
+    <b-card-body>
+      <b-button
+        variant="light"
+        v-if="!_check"
+        size="lg"
+        @click="$emit('change', answer)">
+        Check
+      </b-button>
+      <b-button
+        variant="light"
+        v-if="_check"
+        size="lg"
+        @click="$emit('next')">
+        continue
+      </b-button>
+    </b-card-body>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'word-constructor',
+  props: {
+    _answer: {
+      type: String
+    },
+    _color: {
+      type: String,
+      default: ''
+    },
+    _check: {
+      type: Boolean,
+      default: false
+    },
+    _userAnswer: {
+      type: String
+    },
+    _words: { 
+      type: Array,
+      default: ''
+    },
+    _audio: {
+      type: String,
+      default: ''
+    },
+    _show: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      showAudio: false,
+      answer: '',
+      words: [...this._words] || []
+    }
+  },
+  computed: {
+    isColorDark () {
+      return this.$store.state.colorScheme.isColorDark
+    }
+  },
+  methods: {
+    resetwords () {
+      this.words = [...this._words]
+      this.answer = ''
+    },
+    setword (id) {
+      this.answer += (this.words[id] + ' ')
+      this.words.splice(id, 1)
+    }
+  }
+}
+</script>
---
+++ pages/practice/audition-phrase/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <b-container class="text-center">
+    <practice-card
+      v-if="!start && infoPractice && !loading"
+      :_practiceInfo="infoPractice"
+      :_notEnoughWords="notEnoughWords"
+      :_wrongAnswers="wrongAnswers"
+      @click="getSkyengMeanings" />
+    <b-row class="justify-content-center">
+      <phrase-constructor v-if="start && meanings" 
+        @change="checkAnswer"
+        @next="setMeanId"
+        :_words="getWords(meanings[meanId].definition.text)"
+        :_audio="meanings[meanId].definition.soundUrl || ''"
+        :_show="show"
+        :_userAnswer="userAnswer"
+        :_answer="meanings[meanId].definition.text"
+        :_color="keyColor"
+        :_check="checkPhrase"
+        :key="keyColor" />
+      </b-row>
+  </b-container>
+</template>
+
+<script>
+import practiceCard from '~/components/Practice-card.vue'
+import phraseConstructor from '~/components/phrase-constructor.vue'
+import meaningInfo from '~/components/meaning-info.vue'
+import _ from 'lodash'
+
+export default {
+  name: 'phrase-audio',
+  components: {
+    practiceCard,
+    phraseConstructor,
+    meaningInfo
+  },
+  data () {
+    return {
+      start: false,
+      meanId: 0,
+      keyColor: '',
+      userAnswer: null,
+      wrongAnswers: null,
+      checkPhrase: false,
+      show: {
+        title: true,
+        audio: true,
+        image: true
+      }
+    }
+  },
+  computed: {
+    meanings () {
+      return this.dictionaryWords || this.$store.state.appLogic.meanings || []
+    }
+  },
+  methods: {
+    checkAnswer (answer) {
+      this.checkPhrase = true
+      this.userAnswer = answer
+      if (this.meanings[this.meanId].definition.text == answer.slice(0, -1)) {
+        this.keyColor = 'success'
+      } else {
+        this.keyColor = 'danger'
+        this.setWrongAnswer(this.meanings[this.meanId])
+      }
+    },
+    getWords (phrase) {
+      var words = phrase.split(' ')
+      this.sortArray(words)
+      return words
+    },
+    setMeanId () {
+      if (this.meanings && this.meanId != this.meanings.length - 1) {
+        this.meanId += 1
+        this.keyColor = ''
+        this.checkPhrase = false
+      } else {
+        this.start = false
+        this.keyColor = ''
+        this.checkPhrase = false
+        this.meanId = 0
+      }
+    }
+  },
+  mixins: [
+    require('~/mixins/wordPractice').default
+  ]
+}
+</script>
