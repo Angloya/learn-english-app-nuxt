70621bf622eadd68f08e0e9e7206ca5aa55c02bb
Alena Borisova <alena.borisova@altenar.com>
2019-03-02 20:01:52 +0300

    added new page - word definition

---
+++ components/cards/Card-game-definition.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-card 
+    @click="$emit('click', card.id)"
+    :border-variant="isColorDark ? 'light' : 'dark'"
+    :header="card.Type"
+    :text-variant="isColorDark ? 'light' : 'dark'"
+    :bg-variant="isColorDark ? 'dark' : 'light'"
+    align="center">
+    <p class="card-text">{{card.text}}
+    </p>
+  </b-card>
+</template>
+
+<script>
+
+export default {
+  name: 'card-game-definition',
+  props: {
+    _card: {
+      type: Object
+    }
+  },
+  computed: {
+    isColorDark () {
+      return this.$store.state.colorScheme.isColorDark
+    }
+  }
+}
+</script>
---
+++ pages/games/word-definition/index.vue
@@ -0,0 +1,17 @@
+<template>
+<b-container>
+  <b-card-group deck class="mb-3">
+
+  </b-card-group>
+</b-container>
+</template>
+
+<script>
+import CardGameDefinition from '~/components/cards/card-game-definition.vue'
+export default {
+  name: 'game-word-definition',
+  components: {
+    CardGameDefinition
+  }
+}
+</script>
